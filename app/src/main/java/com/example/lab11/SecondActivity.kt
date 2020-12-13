fc43f4c70ca1f5a33f700ac392a5f888861475f1
Serhii Orshovskyi <sergorshovsky@gmail.com>
2020-12-13 18:41:40 +0200

    Done lab2

--- app/src/main/java/com/example/lab11/SecondActivity.kt
+++ app/src/main/java/com/example/lab11/SecondActivity.kt
@@ -1,11 +1,40 @@
 package com.example.lab11
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+    }
+
+    fun showPreviousActivity(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun showNextActivity(view: View) {
+        val intent = Intent(this, ThirdActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun setDefaultButtonsColor(view: View) {
+        button3.setBackgroundColor(Color.GRAY)
+        button4.setBackgroundColor(Color.GRAY)
+    }
+
+    fun setGreenButtonsColor(view: View) {
+        button3.setBackgroundColor(Color.GREEN)
+        button4.setBackgroundColor(Color.GREEN)
+    }
+
+    fun setBlueButtonsColor(view: View) {
+        button3.setBackgroundColor(Color.BLUE)
+        button4.setBackgroundColor(Color.BLUE)
     }
 }
--- app/src/main/java/com/example/lab11/ThirdActivity.kt
+++ app/src/main/java/com/example/lab11/ThirdActivity.kt
@@ -6,35 +6,37 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_third.*
 
-class MainActivity : AppCompatActivity() {
+
+class ThirdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_third)
+
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            textView5.text = "You have chosen: $dayOfMonth.${month + 1}.$year"
+        }
     }
 
-    fun setLabelText(view: View) {
-        val enteredText = textField.text.toString()
-        textView.text = enteredText
-    }
-
-    fun showSecondActivity(view: View) {
+    fun showPreviousActivity(view: View) {
         val intent = Intent(this, SecondActivity::class.java)
         startActivity(intent)
     }
 
-    fun showAuthorInfo(view: View) {
-        val toast = Toast.makeText(this, "Author: Serhii Orshovskiy", Toast.LENGTH_SHORT)
-        toast.show()
+    fun showNextActivity(view: View) {
+
+    }
+
+    fun clear(view: View) {
+        textView5.text = ""
     }
 
     fun copyToClipBoard(view: View) {
         val clipboard: ClipboardManager =
             getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        val clip: ClipData = ClipData.newPlainText("simple text", textView.text)
+        val clip: ClipData = ClipData.newPlainText("simple text", textView5.text)
         clipboard.setPrimaryClip(clip)
     }
 }
